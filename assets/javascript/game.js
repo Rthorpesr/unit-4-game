887f5e4f3915ddac09b1d8bd871c9a19c81d1c79
Marine809 <rthorpesr@verizon.net>
2019-06-18 16:29:49 -0400

    mixed the adding of the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,81 @@
 
   $(document).ready(function()
   {
-    computernumber();
-    playernumber();
+ 
     var numWins = 0;
     var numLose = 0;
     var totalScore = 0;
 
     $('totalScore').text(totalScore);
   
-    var computerNum = $('#rubyone').val(Math.floor(19 + Math.random()*(120 + 1 - 19)));
-    var rubyone     = $('#rubyone').val(Math.floor(min + Math.random()*(12 + 1 - 1)));
-    var blueone     = $('#rubyone').val(Math.floor(min + Math.random()*(12 + 1 - 1)));
-    var yellowone   = $('#rubyone').val(Math.floor(min + Math.random()*(12 + 1 - 1)));
-    var greenone    = $('#rubyone').val(Math.floor(min + Math.random()*(12 + 1 - 1)));
+    var randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
 
-    console.log("Ruby One: " + $('#rubyone').val());
+    $('#randNum').html(randNumber);
+    
+    var amethyst = Math.floor(Math.random() * 12) + 1;
+    // var rubyone     = $('#rubyone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
+    var blueone     = $('#blueone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
+    var yellowone   = $('#yellowone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
+    var greenone    = $('greenone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
+
+    // console.log("Ruby One: " + $('#rubyone').val());
     console.log("Blue One: " + $('#blueone').val());
     console.log("Yellow One: " + $('#yellowone').val());
     console.log("Green One: " + $('#rubyone').val());
 
      
-     function playerLoses () {
+     function playerLoses() 
+          {
                numlose = losses + 1;
-               $('#numoflosses').text(numlose);
+               $('#losses').text(numlose);
                resetLast ();
                resetfirst();
+          }
+
+      function playerwins() 
+           {
+              numWins = numWins + 1;
+              $('#wins').text(numWins);
+                 resetLast ();
+                 resetfirst();
+           }
+
+      $('#amethyst').click(() =>
+            {
+              totalScore = totalScore + amethyst       
+              $('#totScore').text( totalScore);
+      
+              if ( totalScore === randNumber) 
+                 {
+                    playerwins();  
+                    resetfirst();
+                    resetLast();   
+                  } 
+               else if (totalScore > randNumber) 
+                    {
+                      playerLoses();
+                      resetfirst();
+                      resetLast(); 
+                    }
+            })
+
+            function resetfirst()
+              {
+                totalScore = 0;
+                $('#totScore').text(totalScore);
+              }
+      
+      
+          function resetLast()           
+            {     
+              var randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+
+                   $('#randNum').html(randNumber);
+
+              var amethyst = Math.floor(Math.random() * 12) + 1;
+              // var rubyone     = $('#rubyone').val(Math.floor(min + Math.random()*(12 + 1 - 1)));
+              var blueone     = $('#blueone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
+              var yellowone   = $('#yellowone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
+              var greenone    = $('greenone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
             }
     });
