5bee6188527dac26f0e73726d04f8d39e4c4ad36
Marine809 <rthorpesr@verizon.net>
2019-06-18 22:26:28 -0400

    finished all the conditional statements for each crystal and tested

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,82 +1,127 @@
-
-
-  $(document).ready(function()
-  {
- 
-    var numWins = 0;
-    var numLose = 0;
-    var totalScore = 0;
-
-    $('totalScore').text(totalScore);
-  
-    var randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-
-    $('#randNum').html(randNumber);
-    
-    var amethyst = Math.floor(Math.random() * 12) + 1;
-    // var rubyone     = $('#rubyone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-    var blueone     = $('#blueone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-    var yellowone   = $('#yellowone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-    var greenone    = $('greenone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-
-    // console.log("Ruby One: " + $('#rubyone').val());
-    console.log("Blue One: " + $('#blueone').val());
-    console.log("Yellow One: " + $('#yellowone').val());
-    console.log("Green One: " + $('#rubyone').val());
-
-     
-     function playerLoses() 
-          {
-               numlose = losses + 1;
-               $('#losses').text(numlose);
-               resetLast ();
-               resetfirst();
-          }
-
-      function playerwins() 
-           {
-              numWins = numWins + 1;
-              $('#wins').text(numWins);
-                 resetLast ();
-                 resetfirst();
-           }
-
-      $('#amethyst').click(() =>
-            {
-              totalScore = totalScore + amethyst       
-              $('#totScore').text( totalScore);
+$(document).ready(function()
+{
+  // 1. Create a random number between (19 and 120) for the computer and store in on screen
+      var computernum = Math.floor(Math.random() * ((120 - 19) + 1) + 19);   
+      $('#computerNumber').html(computernum);  console.log("computernumber: " + computernum);
       
-              if ( totalScore === randNumber) 
-                 {
-                    playerwins();  
-                    resetfirst();
-                    resetLast();   
-                  } 
-               else if (totalScore > randNumber) 
-                    {
-                      playerLoses();
-                      resetfirst();
-                      resetLast(); 
-                    }
-            })
+  // 2. create a random number between (1 and 12) for each crystal to use that number during the Game.
+      var rubynum   = Math.floor(Math.random() * 12) + 1; console.log("Ruby number: " + rubynum);
+      var bluenum   = Math.floor(Math.random() * 12) + 1; console.log("Blue number: " + bluenum);
+      var yellownum = Math.floor(Math.random() * 12) + 1; console.log("Yellow number: " + yellownum);                  
+      var greennum  = Math.floor(Math.random() * 12) + 1; console.log("Greennumber: " + greennum);
 
-            function resetfirst()
+  //  3. Variables to hold total score and
+      var totals = 0; var wins = 0; var lost = 0;
+
+  /*  4. Keep a running total of the sum of all the crystals "clicked"
+         everytime a crystal is clicked:the sum of that clicked crystal is added to totalScore
+         (FOR EACH CRYSTAL)
+          - check to see if the crystal sum is equal to the computer number 
+              - if equal
+                   add 1 to the  wins total
+                   reset the computer and crystals random number
+                else 
+                   if crystal sum > computer total
+                      add 1 to losses
+                      reset the computer and crystals random number
+                   else   
+                      if crystal sum < computer total
+                         click a crystal and add to the crystal sum. 
+  */
+      $("#rubyone").on("click", function() 
               {
-                totalScore = 0;
-                $('#totScore').text(totalScore);
+                  totals = totals + rubynum;
+                  $('#score').text(totals);                 console.log("totals = " +totals);
+                  if (totals === computernum)
+                     {
+                         win();
+                         resetgame();
+                     }
+                  else 
+                      if (totals > computernum) 
+                         {
+                           lose();
+                           resetgame();
+                         } 
+               })
+
+      $("#blueone").on("click", function() 
+      {
+        totals = totals + bluenum;
+        $('#score').text(totals);                             console.log("totals = " +totals);
+        if (totals === computernum) 
+           {
+             win();
+             resetgame();
+           }
+        else
+           if (totals > computernum) 
+              {
+                lose();
+                resetgame();
               }
-      
-      
-          function resetLast()           
-            {     
-              var randNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+      })
 
-                   $('#randNum').html(randNumber);
+      $("#yellowone").on("click", function () 
+      {
+        totals = totals + yellownum;
+        $('#score').text(totals);                             console.log("totals = " +totals);
+        if (totals === computernum)
+           {
+              win();
+              resetgame();
+           }
+        else
+            if (totals > computernum)
+              {
+                  lose();
+                  resetgame();
+              }
+      })
 
-              var amethyst = Math.floor(Math.random() * 12) + 1;
-              // var rubyone     = $('#rubyone').val(Math.floor(min + Math.random()*(12 + 1 - 1)));
-              var blueone     = $('#blueone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-              var yellowone   = $('#yellowone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-              var greenone    = $('greenone').val(Math.floor(1 + Math.random()*(12 + 1 - 1)));
-            }
-    });
+      $("#greenone").on("click", function()
+      {
+          totals = totals + greennum;
+          $('#score').text(totals);                          console.log("totals = " +totals);
+          if (totals === computernum) 
+             {
+                 win();
+                 resetgame();
+             }
+          else
+              if (totals > computernum)
+                 {
+                     lose();
+                     resetgame();
+                 }
+      })
+
+      function win() 
+               {
+                  wins = wins + 1;
+                  $('#wins').text(wins);  
+                  resetgame();           
+                  
+               }
+
+      function lose() 
+               {
+                  lost = lost + 1;
+                  $('#lost').text(lost);  
+                          
+                  resetgame();
+               }
+
+      function resetgame() 
+          {
+             totals = 0;
+             $('#score').text(totals);
+              var computernum = Math.floor(Math.random() * ((120 - 19) + 1) + 19); 
+              $('#computerNumber').html(computernum); 
+              var rubynum   = Math.floor(Math.random() * 12) + 1; console.log("Ruby number: " + rubynum);
+              var bluenum   = Math.floor(Math.random() * 12) + 1; console.log("Blue number: " + bluenum);
+              var yellownum = Math.floor(Math.random() * 12) + 1; console.log("Yellow number: " + yellownum);                  
+              var greennum  = Math.floor(Math.random() * 12) + 1; console.log("Greennumber: " + greennum);
+          }       
+});
+
